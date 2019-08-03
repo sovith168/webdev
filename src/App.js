2a0bbd385d20e4ea82b88c5471c8ada2cd24d658
sovith168 <saksovith99@gmail.com>
2019-08-03 16:39:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Todos from "./conponent/Todos";
+
+function App() {
+  return (
+    <div className="Apps">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conponent/Todos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Todos() {
+  return (
+    <div className="Apps">
+      <h1>Todos </h1>
+    </div>
+  );
+}
+
+export default Todos;
